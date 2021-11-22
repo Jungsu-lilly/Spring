e59f0d21c436ecbdda6be31802bb3eb3a2cb2b12
matt1235 <matt1235@naver.com>
2021-11-22 19:19:11 +0900

    회원 가입, 회원 목록 출력 기능 구현.

--- src/main/java/hello/hellospring/controller/MemberController.java
+++ src/main/java/hello/hellospring/controller/MemberController.java
@@ -1,8 +1,14 @@
 package hello.hellospring.controller;
 
+import hello.hellospring.domain.Member;
 import hello.hellospring.service.MemberService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.List;
 
 @Controller
 public class MemberController {
@@ -12,5 +18,31 @@ public class MemberController {
     @Autowired
     public MemberController(MemberService memberService) {
         this.memberService = memberService;
+    }
+
+
+    /* 회원가입 메뉴 */
+    @GetMapping("/members/new")
+    public String createForm(){
+        return "members/createMemberForm";
+    }
+
+    @PostMapping("/members/new")
+    public String create(MemberForm form){
+        Member member = new Member();
+        member.setName(form.getName());
+
+   //   System.out.println("member : "+form.getName());
+        memberService.join(member);
+
+        return "redirect:/"; // 회원 가입이 끝났으니 홈 화면으로 돌려보내는 것.
+    }
+
+    @GetMapping("/members")
+    public String list(Model model){
+
+        List<Member> members = memberService.findMembers(); // 모든 멤버 조회
+        model.addAttribute("members", members);
+        return "members/memberList";
     }
 }
---
+++ src/main/java/hello/hellospring/controller/MemberForm.java
@@ -0,0 +1,14 @@
+package hello.hellospring.controller;
+
+// 멤버를 등록하는 컨트롤러
+public class MemberForm {
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
